33bcbebec2555fb5911b53259943c2ef6619f56b
Shrikant Kale <shrikale32@gmail.com>
2023-11-15 22:29:35 -0500

    updated Intro text to resolve issue

--- components/intro.tsx
+++ components/intro.tsx
@@ -57,7 +57,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Shrikant.</span> I am a{" "}
+        <span className="font-bold">Hello, I am Shrikant.</span> I am a{" "}
         <span className="font-bold">Full-Stack developer</span> with over{" "}
         <span className="font-bold">3 years</span> of experience. I enjoy
         building <span className="italic">applications end-to-end.</span>
